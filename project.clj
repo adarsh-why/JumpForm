27e55bc1cc106799dbb1406dbd98e4fb2ec245e0
p4v4n <pavan.barcelona@gmail.com>
2018-01-26 15:52:13 +0530

    Add a few handlers + routes   -add composure for routing   -add wrap-params to parse query-strings   -add dump handler to print the request   -write a middleware fn to add 'server' header to response

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring "1.6.3"]]
+                 [ring "1.6.3"]
+                 [compojure "1.6.0"]]
   :main ^:skip-aot jump-form.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
--- src/jump_form/core.clj
+++ src/jump_form/core.clj
@@ -1,17 +1,35 @@
 (ns jump-form.core
   (:gen-class)
   (:require [ring.adapter.jetty :as jetty]
-            [ring.middleware.reload :refer [wrap-reload]]))
+            [ring.middleware.reload :refer [wrap-reload]]
+            [ring.middleware.params :refer [wrap-params]]
+            [compojure.core :refer [defroutes ANY GET POST PUT DELETE]]
+            [compojure.route :refer [not-found]]
+            [ring.handler.dump :refer [handle-dump]]))
 
 (defn hello [req]
   {:status 200
    :body "Hello!"
    :headers {}})
 
+(defroutes routes
+  (GET "/" [] hello)
+  (GET "/request" [] handle-dump)
+  (not-found "Page not found."))
+
+(defn wrap-server [hdlr]
+  (fn [req]
+    (assoc-in (hdlr req) [:headers "Server"] "jump-form")))
+
+(def app
+  (wrap-server
+    (wrap-params
+      routes)))
+
 (defn -main [port]
-  (jetty/run-jetty hello
+  (jetty/run-jetty app
                    {:port (Integer. port)}))
 
 (defn -dev-main [port]
-  (jetty/run-jetty (wrap-reload #'hello)
+  (jetty/run-jetty (wrap-reload #'app)
                    {:port (Integer. port)}))
