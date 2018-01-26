cfa4c7439220a9014ccf55366eec1e0c8b39488a
Sindhu <indusfiretiger@gmail.com>
2018-01-27 03:19:10 +0530

    Add Submit functionality

--- resources/static/form.js
+++ resources/static/form.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function () {
 		var data  = getData();
 });
@@ -17,7 +18,7 @@ var sliceData = function (data) {
 };
 
 var showQuestion = function (que, qid) {
-    $qBody = $('<div class="'+qid+'">'+que+'<div class="'+qid+'-answer"></div></div><br><br>').appendTo('#question');
+    $qBody = $('<div id="'+qid+'">'+que+'<div class="'+qid+'-answer"></div></div><br><br>').appendTo('#question');
 };
 
 var showOptions = function (ans, qid) {
@@ -25,3 +26,32 @@ var showOptions = function (ans, qid) {
        $qBody.find('.'+qid+'-answer').append('<div class="'+qid+'-'+ans[item]+'"><input type="radio" id="'+qid+'_ans_'+item+'"value = '+item+'>'+ans[item]+'</div>');
     }
 };
+function submit(){
+	var uuid = window.location.pathname.split( '/' )[1];
+    let answers = []
+		var radio
+		var q_count = 0
+		var nxtQ = document.getElementById("q"+q_count)
+    while(nxtQ != undefined) {
+			var op_count = 0
+			var nxtOp = document.getElementById("q"+q_count+"_ans_"+op_count)
+			while(nxtOp != undefined){
+				let radio = document.getElementById("q"+q_count+"_ans_"+op_count).checked
+				if(radio == true){
+          let qaPair = [q_count,op_count]
+          answers[q_count] = qaPair
+					break
+        }
+				op_count++
+				var nxtOp = document.getElementById("q"+q_count+"_ans_"+op_count)
+			}
+			q_count++
+			nxtQ = document.getElementById("q"+q_count)
+    }
+		console.log(answers);
+		console.log('uuid', uuid);
+		console.log("json ",JSON.stringify(answers));
+		$.post("post-result/"+uuid,JSON.stringify(answers),function(data,status){
+            alert("Data: " + data + "\nStatus: " + status);
+        });
+}
